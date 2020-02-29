86a88ff1b990dcdf73ff015a23cc07f58ef0d625
xiangsong-yang <55978984+xiangsong-yang@users.noreply.github.com>
2020-02-29 20:31:29 +0000

    Update ofApp.cpp

--- build/src/ofApp.cpp
+++ build/src/ofApp.cpp
@@ -1,165 +1,227 @@
 #include "ofApp.h"
 
+//--------------------------------------------------------------
+
 void ofApp::setup(){
     ofSetFrameRate(24);
     
-    // ------------- midi yilin -------------
-    myImage.load("test2.jpeg");
+    //----
     
-    
-    
-    midi.openPort(0);
-    //    "Impact LX61+ (MIDI1)"
+    sender.setup(HOST, PORT);
+
+    midi.listInPorts();
+    midi.openPort(1);
+    //    midi.openPort("Impact LX61+ (MIDI1)");
     midi.addListener(this);
-    //    myImage.load("test2.jpg");
-    myImage.setImageType(OF_IMAGE_COLOR_ALPHA);
-    //    ofLoadImage(pxdata, "test2.jpg");//load the pixel value into pxdata
-    //    ofLoadImage(pxempty, "test2.jpg");
-    //
+    setShader();
     
-    
-    //       imgw = myImage.getWidth();//width
-    //       imgh = myImage.getHeight();//height
-    ofBackground(20, 20, 20);
 }
 
+//--------------------------------------------------------------
 void ofApp::update() {
-    // ------------- Shader loop moritz -------------
-    // shader.load("3/shader");
     
-    if (ofGetFrameNum()%100 == 0) {
-        shader.unload();
-        ofLog() << i;
-        setShader();
-        if (!shader.isLoaded()) {
-            i = 0;
-            setShader();
+    if (numPressed == 0) {
+        //        std::cout<< "released\n";
+        if (ofGetElapsedTimef() - timer > 3.1 && ofGetElapsedTimef() - timer < 3.12) {
+            //        std::cout<<"sum_of_vel" << sum_of_vel<< "   \n";
+            sum_of_vel = 0;
+            defaultScene();
         }
-        i++;
     }
     
-    
-    // ------------- midi yilin -------------
-    if( positionXs.size() == 0){
-        for (int i = 0; i <20; i++){
-            positionXs.push_back(ofRandom(100,ofGetWidth()-100));
-            positionYs.push_back(ofRandom(100, ofGetHeight()-100));
-            // positionXs.push_back(ofRandom(100,imgw-100));
-            // positionYs.push_back(ofRandom(100,imgh-100));
-            radiuses.push_back(ofRandom(5,70));
-            alphas.push_back(ofRandom(100, 200));
-            timers.push_back(ofGetElapsedTimeMillis());
-        }
-        
-    }
 }
+void ofApp::nextshader(){
+   
+                shader.unload();
+                ofLog() << shaderNumber;
+                setShader();
+                
+                if (!shader.isLoaded()) {
+                    shaderNumber = 0;
+                    setShader();
+                }
+                shaderNumber++;
+            }
+     
+    
 
+//--------------------------------------------------------------
 void ofApp::draw(){
-    // ------------- shader loop moritz -------------
-    ofSetColor(120);
+    ofSetColor(240);
     shader.begin();
     shader.setUniform1f("time", ofGetElapsedTimef());
-    shader.setUniform2f("mouse", mouseX, mouseY);
-    shader.setUniform2f("resolution", ofGetWidth(), ofGetHeight());
+      shader.setUniform2f("mouse", mouseX, mouseY);
+      shader.setUniform2f("resolution", ofGetWidth(), ofGetHeight());
     ofDrawRectangle(0, 0, ofGetWidth(), ofGetHeight());
     shader.end();
     
-    // ------------- midi yilin -------------
-    //    myImage.draw(0, 0);
     
-    for (int i = 0; i < positionYs.size(); i++){
-        ofColor color = myImage.getColor(positionXs[i], positionYs[i]);
-        int alpha = 200 -  (ofGetElapsedTimeMillis()-timers[i]) / 35;
-        if (alpha <0){
-            positionXs.erase(positionXs.begin());
-            positionYs.erase(positionYs.begin());
-            radiuses.erase(radiuses.begin());
-            timers.erase(timers.begin());
-            timecontrol = ofGetElapsedTimeMillis();
-            std::cout << "size   "<<positionXs.size() << "\n";
-        }
-        //            int a;
-        //            if (alpha < 0){
-        //                a = (-alpha%200);
-        //            }
-        //            if(alpha >= 0){
-        //                a = alpha;
-        //            }
-        ofSetColor(color, alpha);
-        ofDrawCircle(positionXs[i], positionYs[i], radiuses[i]);//velocity?
-        std:: cout <<timers[i] <<"   "<< ofGetElapsedTimeMillis()<<"alpha  "<< alpha <<" \n";
-    }
-    
-    if(ofGetElapsedTimeMillis() - timecontrol>1500 && positionXs.size() > 0){
-        positionYs.erase(positionYs.begin());
-        positionXs.erase(positionXs.begin());
-        radiuses.erase(radiuses.begin());
-        timers.erase(timers.begin());
-        timecontrol = ofGetElapsedTimeMillis();
-        std::cout << "size   " << positionXs.size() << "\n";
+    ///----
+    for (int i = 0; i < messages.size(); i++) {
+        ofxMidiMessage & message = messages[i];
+
     }
 }
 
-void ofApp::exit(){
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+    if (key == 'd' || key == 'D') {
+        defaultScene();
+    }
+    if (key == 'l' || key == 'L') {
+        pressLong();
+    }
+    if (key == 'h' || key == 'H') {
+        pressHigh();
+    }
+    if (key == 'F' || key == 'f') {
+        pressFast();
+    }
+    
+}
+
+void ofApp::exit() {
     midi.closePort();
     midi.removeListener(this);
 }
+//--------------------------------------------------------------
 
 void ofApp::newMidiMessage(ofxMidiMessage & message) {
-    x = floor((message.pitch-34) * ofGetWidth()/72 + ofRandom(ofGetWidth()/10));
-    y = floor((message.pitch-34) * ofGetHeight()/290 + ofRandom(ofGetHeight()/1.1));
-    positionXs.push_back(x);
-    positionYs.push_back(y);
-    timecontrol = ofGetElapsedTimeMillis();
-    timers.push_back(ofGetElapsedTimeMillis());
-    radiuses.push_back(floor(message.velocity)/1.6+15);
-    
-    if (messages.size() > 128){
-        positionYs.erase(positionYs.begin());
-        positionXs.erase(positionXs.begin());
-        radiuses.erase(radiuses.begin());
-        timers.erase(timers.begin());
+    messages.push_back(message);
+    if (messages.size() > 64) {
+        messages.erase(messages.begin());
     }
-    std::cout << "pitch:   " << message.pitch << "   velocity:   " << message.velocity << "   \n";
-}
 
-void ofApp::keyPressed(int key){
+    if (message.velocity > 50) {
+        sum_of_vel += 1;
+    }
+    sum_of_vel += message.velocity;
+
+
+    if (sum_of_vel > 500) {
+        //time time time ofGetElapsedTimef()
+        pressFast();
+    }
+
+    if (message.pitch >= 84) {
+        pressHigh();
+    }
+
+
+    if (message.status == MidiStatus::MIDI_NOTE_ON) {
+        numPressed += 1;
+        timer = ofGetElapsedTimef();
+    }
+    if (message.status == MidiStatus::MIDI_NOTE_OFF) {
+        numPressed -= 1;
+        timer = ofGetElapsedTimef();
+    std:cout << "timer = ofGetElapsedTimef()\n";
+
+    }
+    if (numPressed >= 5) {
+        pressLong();
+    }
     
-}
 
+}
 void ofApp::keyReleased(int key){
-    
+
 }
 
+//--------------------------------------------------------------
 void ofApp::mouseMoved(int x, int y){
     
 }
 
+//--------------------------------------------------------------
 void ofApp::mouseDragged(int x, int y, int button){
-    
+
 }
 
+//--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-    
+
 }
 
+//--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
-    
+
 }
 
+//--------------------------------------------------------------
 void ofApp::windowResized(int w, int h){
-    myImage.resize(w, h);
+
 }
 
+//--------------------------------------------------------------
 void ofApp::gotMessage(ofMessage msg){
-    
+
 }
 
+//--------------------------------------------------------------
 void ofApp::dragEvent(ofDragInfo dragInfo){
-    
+
 }
 
+//--------------------------------------------------------------
 void ofApp::setShader () {
-    string path = std::to_string(i) + "/shader";
+    string path = std::to_string(shaderNumber) + "/shader";
     shader.load(path);
+}
+void ofApp::defaultScene() {
+    ofxOscMessage m;
+    m.setAddress("/stop");
+    m.addIntArg(87);
+    sender.sendMessage(m);
+    m.setAddress("/campfire");
+    //    m.addStringArg( "campfire" );
+    m.addIntArg(78);
+    std::cout << "default\n";
+    sender.sendMessage(m);
+}
+
+void ofApp::pressHigh() {
+    ofxOscMessage m;
+    m.setAddress("/highNotes");
+    //    m.addStringArg( "high" );
+    m.addIntArg(6);
+    std::cout << "high\n";
+    sender.sendMessage(m);
+}
+void ofApp::pressLong() {
+    ofxOscMessage m;
+    m.setAddress("/stop");
+    m.addIntArg(87);
+    sender.sendMessage(m);
+
+    m.setAddress("/longNotes");
+    //    m.addStringArg( "long" );
+    m.addIntArg(4);
+    std::cout << "long\n";
+    sender.sendMessage(m);
+
+
+
+
+}
+void ofApp::pressFast() {
+    ofxOscMessage m;
+    m.setAddress("/stop");
+    m.addIntArg(87);
+    sender.sendMessage(m);
+
+    m.setAddress("/fastMode");
+    m.addIntArg(3);
+    std::cout << "fastMode\n";
+    sender.sendMessage(m);
+
+    //    sum_of_vel = 0;
+}
+
+void ofApp::pressNormal() {
+    ofxOscMessage m;
+    m.setAddress("/normalPress");
+    m.addIntArg(3);
+    std::cout << "normalPress\n";
+    sender.sendMessage(m);
 }
